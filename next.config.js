7124bdbe480f4816e5531e397a43fb64e9736d1a
Saknarong Sombutjaroan <blackpotter@Saknarongs-MacBook-Air.local>
2023-12-31 13:56:53 +0700

    Update Next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
 
 module.exports = nextConfig
