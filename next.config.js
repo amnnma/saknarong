ddb5cbbbe2aa8519473d08c2ed2752d5aa3f38ee
Saknarong <blackpotter@Saknarongs-MacBook-Air.local>
2024-06-27 22:20:15 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     output: 'export',
     trailingSlash: true,
     skipTrailingSlashRedirect: true,
+    ignoreDuringBuilds: true,
     images: { unoptimized: true } 
     // Optional: Change the output directory `out` -> `dist`
     // distDir: 'dist',
